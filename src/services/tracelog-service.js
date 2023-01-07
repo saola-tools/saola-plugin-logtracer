7ecd76f77ef5a0c16fe1f13ed6167bdc586acd6d
pnhung177 <pnhung177@gmail.com>
2023-01-07 17:06:45 +0700

    Support multiple portlets

--- src/services/tracelog-service.js
+++ src/services/tracelog-service.js
@@ -1,20 +1,21 @@
 "use strict";
 
 const Devebot = require("devebot");
+const chores = Devebot.require("chores");
 const lodash = Devebot.require("lodash");
 
-function TracelogService (params = {}) {
-  const { loggingFactory, sandboxConfig, webweaverService } = params;
-  const L = loggingFactory.getLogger();
-  const T = loggingFactory.getTracer();
+const { DEFAULT_PORTLET_NAME, standardizeConfig } = require("app-webserver").require("portlet");
 
-  const tracingRequestName = sandboxConfig.tracingRequestName || "requestId";
+function TracelogPortlet (params = {}) {
+  const { L, T, portletConfig, portletName, portletForwarder } = params;
+
+  const tracingRequestName = portletConfig.tracingRequestName || "requestId";
 
   this.getRequestId = function(req) {
     return req && req[tracingRequestName];
   };
 
-  let tracingPaths = lodash.get(sandboxConfig, ["tracingPaths"]);
+  let tracingPaths = lodash.get(portletConfig, ["tracingPaths"]);
   if (lodash.isString(tracingPaths)) tracingPaths = [ tracingPaths ];
   if (!lodash.isArray(tracingPaths)) tracingPaths = [];
 
@@ -51,7 +52,7 @@ function TracelogService (params = {}) {
 
   let requestInterceptor = function(req, res, next) {
     req[tracingRequestName] = req[tracingRequestName] ||
-        req.get(sandboxConfig.tracingRequestHeader) || req.query[tracingRequestName];
+        req.get(portletConfig.tracingRequestHeader) || req.query[tracingRequestName];
     //
     if (!req[tracingRequestName]) {
       req[tracingRequestName] = T.getLogID();
@@ -62,7 +63,7 @@ function TracelogService (params = {}) {
       }));
     }
     //
-    res.setHeader(sandboxConfig.tracingRequestHeader, req[tracingRequestName]);
+    res.setHeader(portletConfig.tracingRequestHeader, req[tracingRequestName]);
     L.has("info") && L.log("info", T.add({
       requestId: req[tracingRequestName]
     }).toMessage({
@@ -82,17 +83,56 @@ function TracelogService (params = {}) {
   };
 
   this.push = function(layerOrBranches, priority) {
-    priority = (typeof(priority) === "number") ? priority : sandboxConfig.priority;
-    webweaverService.push(layerOrBranches, priority);
+    if (portletForwarder.hasPortlet(portletName)) {
+      priority = (typeof(priority) === "number") ? priority : portletConfig.priority;
+      portletForwarder.getPortlet(portletName).push(layerOrBranches, priority);
+    }
   };
 
-  if (sandboxConfig.autowired !== false) {
+  if (portletConfig.autowired !== false && portletForwarder.hasPortlet(portletName)) {
     let layers = [ this.getTracingListenerLayer() ];
-    if (sandboxConfig.tracingBoundaryEnabled) {
+    if (portletConfig.tracingBoundaryEnabled) {
       layers.push(this.getTracingBoundaryLayer());
     }
-    webweaverService.push(layers, sandboxConfig.priority);
+    portletForwarder.getPortlet(portletName).push(layers, portletConfig.priority);
   }
+}
+
+function TracelogService (params = {}) {
+  const { packageName, loggingFactory, sandboxConfig, webweaverService } = params;
+  const L = loggingFactory.getLogger();
+  const T = loggingFactory.getTracer();
+  const blockRef = chores.getBlockRef(__filename, packageName || "app-webweaver");
+
+  const pluginConfig = standardizeConfig(sandboxConfig);
+  const portletForwarder = webweaverService;
+
+  const _portlets = {};
+  lodash.forOwn(pluginConfig.portlets, function(portletConfig, portletName) {
+    _portlets[portletName] = new TracelogPortlet({ L, T, blockRef, portletConfig, portletName, portletForwarder });
+  });
+
+  this.getPortletNames = function() {
+    return lodash.keys(_portlets);
+  };
+
+  this.hasPortlet = function(portletName) {
+    portletName = portletName || DEFAULT_PORTLET_NAME;
+    if (!portletForwarder.hasPortlet(portletName)) {
+      return false;
+    }
+    return portletName in _portlets;
+  };
+
+  this.getPortlet = function(portletName) {
+    portletName = portletName || DEFAULT_PORTLET_NAME;
+    return _portlets[portletName];
+  };
+
+  // @deprecated
+  this.push = function(layerOrBranches, priority) {
+    return this.hasPortlet() && this.getPortlet().push(layerOrBranches, priority) || undefined;
+  };
 };
 
 TracelogService.referenceHash = {
